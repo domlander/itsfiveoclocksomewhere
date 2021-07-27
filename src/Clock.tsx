dc0216aa7830e951374d1118abb63cd8ec64cdd5
Dominik Lander <lander45@hotmail.co.uk>
2021-07-27 20:42:03 +0100

    Initial commit

---
+++ src/Clock.tsx
@@ -0,0 +1,29 @@
+
+import React, { Component, useEffect, useState } from 'react';
+import styles from '../styles/Clock.module.css'
+
+interface Props {
+  hoursUntil5pm: number
+}
+
+const timeToDisplay = (hours: number) => {
+  const now = new Date()
+  now.setHours((now.getHours() + hours) % 24)
+  return now.toLocaleTimeString()
+}
+
+const Clock = ({ hoursUntil5pm }: Props) => {
+  const [clock, setClock] = useState(timeToDisplay(hoursUntil5pm));
+
+  useEffect(() => {
+    let id = setInterval(() => {
+      setClock(timeToDisplay(hoursUntil5pm));
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, [hoursUntil5pm]);
+
+  return <h2 className={styles.h2}>{clock}</h2>;
+}
+
+export default Clock
---
+++ src/ImageLookUp.ts
@@ -0,0 +1,32 @@
+interface Dictionary {
+  [Key: number]: [string, string];
+}
+
+const ImageLookUp: Dictionary = {
+  0: ["London", "London-sunset.svg"], // 4pm GMT
+  1: ["Paris", "France-sunset.svg"],
+  2: ["Nairobi", "Desert-sunset.svg"],
+  3: ["Dubai", "Desert-sunset.svg"],
+  4: ["Mumbai", "India-sunset.svg"], // 8pm GMT
+  5: ["Omsk", "Snow-sunset.svg"],
+  6: ["Jakarta", "Indonesia-sunset.svg"],
+  7: ["Beijing", "China-sunset.svg"], // 12am GMT
+  8: ["Tokyo", "ItsukishimaGateJapan-sunset.svg"],
+  9: ["Sydney", "India-sunset.svg"],
+  10: ["Srednekolymsk", "Snow-sunset.svg"],
+  11: ["Auckland", "Valley-day.svg"],
+  12: ["Tonga", "tropical-sunset.svg"], // 4am GMT
+  13: ["Honolulu", "tropical-sunset.svg"],
+  14: ["Adak", "Snow-sunset.svg"],
+  15: ["Alaska", "Snow-sunset.svg"],
+  16: ["Los Angeles", "City-sunset.svg"], // 8am GMT
+  17: ["Denver", "HillsandMountains-sunset.svg"],
+  18: ["Chicago", "Lake-sunset.svg"],
+  19: ["New York", "USA-sunset.svg"],
+  20: ["Brasilia", "Forest-sunset.svg"], // 12pm GMT
+  21: ["Greenland", "Snow-sunset.svg"],
+  22: ["Cape Verde", "SeaLighthouse-sunset.svg"],
+  23: ["Timbuktu", "Desert-sunset.svg"],
+}
+
+export default ImageLookUp;
