60d5aff5c8a89849922e5a64138c83f63b4ec78d
Dominik Lander <lander45@hotmail.co.uk>
2021-07-27 21:04:20 +0100

    Use UTC time to get GMT time, not local time

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Clock from '../src/Clock'
-import ImageDictionary from "../src/ImageLookUp"
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Clock from "../src/Clock";
+import ImageDictionary from "../src/ImageLookUp";
 
 export default function Home() {
-  const hours = new Date().getHours();
+  const hours = new Date().getUTCHours() + 1;
   const hoursUntil5pm = (24 + 17 - hours) % 24;
-  const location = ImageDictionary[hoursUntil5pm]
+  const location = ImageDictionary[hoursUntil5pm];
 
   return (
     <div className={styles.container}>
-      <Image src={`/landscapes/${location[1]}`} alt="" layout="fill" objectFit="cover" />
+      <Image
+        src={`/landscapes/${location[1]}`}
+        alt=""
+        layout="fill"
+        objectFit="cover"
+      />
       <div className={styles.opacityLayer} />
       <div className={styles.content}>
         <p className={styles.p}>It&rsquo;s five o&rsquo;clock in...</p>
@@ -19,5 +24,5 @@ export default function Home() {
         <Clock hoursUntil5pm={hoursUntil5pm} />
       </div>
     </div>
-  )
+  );
 }
--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,16 +1,15 @@
-
-import React, { Component, useEffect, useState } from 'react';
-import styles from '../styles/Clock.module.css'
+import React, { useEffect, useState } from "react";
+import styles from "../styles/Clock.module.css";
 
 interface Props {
-  hoursUntil5pm: number
+  hoursUntil5pm: number;
 }
 
 const timeToDisplay = (hours: number) => {
-  const now = new Date()
-  now.setHours((now.getHours() + hours) % 24)
-  return now.toLocaleTimeString()
-}
+  const now = new Date();
+  now.setHours((now.getHours() + hours) % 24);
+  return now.toLocaleTimeString();
+};
 
 const Clock = ({ hoursUntil5pm }: Props) => {
   const [clock, setClock] = useState(timeToDisplay(hoursUntil5pm));
@@ -24,6 +23,6 @@ const Clock = ({ hoursUntil5pm }: Props) => {
   }, [hoursUntil5pm]);
 
   return <h2 className={styles.h2}>{clock}</h2>;
-}
+};
 
-export default Clock
+export default Clock;
