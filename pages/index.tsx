2a71467563428ae433c9127f06e60a951bbb521f
Dominik Lander <lander45@hotmail.co.uk>
2021-08-27 13:50:13 +0100

    Stop caching

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,6 +32,12 @@ export default function Home() {
         />
         <meta property="og:image:width" content="750" />
         <meta property="og:image:height" content="500" />
+        <meta
+          httpEquiv="Cache-Control"
+          content="no-cache, no-store, must-revalidate"
+        />
+        <meta httpEquiv="Pragma" content="no-cache" />
+        <meta httpEquiv="Expires" content="0" />
       </Head>
       <div className={styles.container}>
         <Image
