ee62039aef91a6959f383f8dcc066f66fdebadac
Dominik Lander <dominik.lander@avantarte.com>
2022-02-25 12:09:28 +0000

    Add lang to html element

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,7 +14,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Varela+Round&display=optional"
