a56242a4eb49a68cf9b115d53cf6719c7545d49d
Dominik Lander <lander45@hotmail.co.uk>
2022-03-01 17:05:40 +0000

    Added comments and try catch to the fetching of locations from the DB

--- pages/api/getCurrentLocation.ts
+++ pages/api/getCurrentLocation.ts
@@ -10,16 +10,29 @@ type Error = {
   error: string;
 };
 
+/*
+    1. Retrieves all the locations we have in the database
+    2. filters to find matching locations
+    3. Returns found matching locations
+*/
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data | Error>
 ) {
   const client = await clientPromise;
-  const locations = await client
-    .db("itsfiveoclocksomewhere")
-    .collection("locations")
-    .find()
-    .toArray();
+  let locations;
+
+  try {
+    locations = await client
+      .db("itsfiveoclocksomewhere")
+      .collection("locations")
+      .find()
+      .toArray();
+  } catch (e) {
+    return res.status(500).json({
+      error: `Fetching locations from the database failed. ${e}`,
+    });
+  }
 
   const hoursUntil5pm = parseInt(req.query.hoursUntil5pm as string);
   const matchingLocations = locations.filter(
@@ -28,10 +41,13 @@ export default async function handler(
 
   const location = matchingLocations[0] || null;
   if (!location) {
-    return res
-      .status(500)
-      .json({ error: "failed to find a suitable location" });
+    return res.status(500).json({
+      error: "failed to find a location where it is between 5pm and 6pm",
+    });
   }
 
-  return res.status(200).json({ name: location.name, image: location.image });
+  return res.status(200).json({
+    name: location.name,
+    image: location.image,
+  });
 }
