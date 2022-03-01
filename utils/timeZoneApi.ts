4ac55fb2376580bad18814c743df323057c893ab
Dominik Lander <lander45@hotmail.co.uk>
2022-03-01 17:28:24 +0000

    Fix error message copy

--- utils/timeZoneApi.ts
+++ utils/timeZoneApi.ts
@@ -10,7 +10,7 @@ const API_URL = `http://api.timezonedb.com/v2.1/list-time-zone?key=${process.env
 
 export const getApiLocationData = async () => {
   if (!process.env.TIMEZONEDB_API_KEY) {
-    throw new Error("Please add your Mongo URI to .env.local");
+    throw new Error("Please add your TIMEZONEDB_API_KEY to .env.local");
   }
 
   const locations = await axios.get(API_URL).then(({ data }) =>
